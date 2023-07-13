79da4eefb4848032a5c708e9bea9f95b7350d08f
Tomas Aftalion <t@pachama.com>
2023-07-13 20:52:48 +0000

    Able to swap sample_farthest_points for pointnet2 implementation

--- utils/misc.py
+++ utils/misc.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import os
 from collections import abc
 from pointnet2_ops import pointnet2_utils
+from pytorch3d.ops import sample_farthest_points
 
 
 def fps(data, number):
@@ -15,8 +16,9 @@ def fps(data, number):
         data B N 3
         number int
     '''
-    fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
-    fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
+    fps_data, fps_index = sample_farthest_points(points=data, K=number)
+    #fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
+    #fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
     return fps_data
 
 
